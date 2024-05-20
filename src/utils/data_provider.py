07a89f98f2243d23ba55b9ade6dd110f8e626cc8
Tobias Lindenbauer <tobiaslindenbauer98@gmail.com>
2024-05-20 13:26:46 +0200

    Adjusted DataProvider provide_data_as_numpy_array method signature to allow dynamic data loading from n files or connections

--- src/utils/data_provider.py
+++ src/utils/data_provider.py
@@ -6,15 +6,18 @@ from abc import ABCMeta
 
 
 class DataProvider(metaclass=ABCMeta):
+    """
+    Defines the interfaces for a data provider that reads a lexical simplification dataset from a file and returns it as
+    a numpy array for further processing. Note that the variables relating to the path to the dataset should be
+    stored as attributes in the implementing subclass and accessed this way. This is to allow for the flexibility of
+    changing the path to the dataset without changing the method signature.
+    """
 
     @abc.abstractmethod
-    def provide_data_as_numpy_array(self, filename: str) -> np.ndarray:
+    def provide_data_as_numpy_array(self) -> np.ndarray:
         """
         Reads the data from the given file and returns it as a numpy array. The format the data that is returned
         must conform to is specified in the documentation of the return value of this method.
-
-        Args:
-            filename: The path to the file containing the data.
 
         Returns:
             A numpy array where each row corresponds to a datapoint from a lexical simplification dataset.
