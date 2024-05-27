4b748175830f110cdc961885093462b4e7911a2a
Tobias Lindenbauer <tobiaslindenbauer98@gmail.com>
2024-05-27 09:10:03 +0200

    Initialized data providers and datasets

--- src/benchmark_suite.py
+++ src/benchmark_suite.py
@@ -2,21 +2,27 @@ from typing import List, Dict
 
 from lexical_simplifier import LexicalSimplifier
 from utils.data_provider import DataProvider
+from utils.bench_ls_data_provider import BenchLSDataProvider
+from utils.germaneval_data_provider import GermanEvalDataProvider
 from language import Language
 
 
 class BenchmarkSuite:
-
     testee_model: LexicalSimplifier = None
     languages: List[Language] = None
-    datasets: Dict[Language: List[DataProvider]] = {
-        Language.DE: ["germeval, germeval2015"],
-        Language.EN: ["benchls"]
-    }
+    datasets: Dict[Language: List[DataProvider]] = None
 
     def __init__(self, testee_model: LexicalSimplifier, languages: List[Language]):
         self.testee_model = testee_model
         self.languages = languages
+
+        self.datasets = {
+            Language.DE: [GermanEvalDataProvider(
+                '/content/drive/MyDrive/nlp_ss24/multilingual-lexical-simplification/data/germeval/train-dataset.xml',
+                '/content/drive/MyDrive/nlp_ss24/multilingual-lexical-simplification/data/germeval/train-dataset.gold')],
+            Language.EN: [BenchLSDataProvider(
+                '/content/drive/MyDrive/nlp_ss24/multilingual-lexical-simplification/data/BenchLS/BenchLS.txt')]
+        }
 
     def run(self):
         # run the benchmark suite with the specified config (ie attrs)
@@ -45,4 +51,3 @@ class BenchmarkSuite:
     def disable_datasets(self, datasets: List[str]):
         # disable multiple datasets from the benchmark suite
         pass
-
