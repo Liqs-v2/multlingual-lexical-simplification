71958ad0a75329acc61bff6ef23c7a2a78e9b547
Tobias Lindenbauer <tobiaslindenbauer98@gmail.com>
2024-05-27 09:01:15 +0200

    Init datasets, refactored languages to enum type

--- src/benchmark_suite.py
+++ src/benchmark_suite.py
@@ -2,34 +2,39 @@ from typing import List, Dict
 
 from lexical_simplifier import LexicalSimplifier
 from utils.data_provider import DataProvider
+from language import Language
 
 
 class BenchmarkSuite:
 
     testee_model: LexicalSimplifier = None
-    languages: List[str] = None # TODO transform str into enum for robustness
-    datasets: Dict[str: List[DataProvider]] = None
+    languages: List[Language] = None
+    datasets: Dict[str: List[DataProvider]] = {
+        "de": ["germeval, germeval2015"],
+        "en": ["benchls"]
+    }
 
-    def __init__(self):
-        pass
+    def __init__(self, testee_model: LexicalSimplifier, languages: List[Language]):
+        self.testee_model = testee_model
+        self.languages = languages
 
     def run(self):
         # run the benchmark suite with the specified config (ie attrs)
         pass
 
-    def add_language(self, language: str):
+    def add_language(self, language: Language):
         # add a language to the benchmark suite
         pass
 
-    def add_languages(self, languages: List[str]):
+    def add_languages(self, languages: List[Language]):
         # add multiple languages to the benchmark suite
         pass
 
-    def remove_language(self, language: str):
+    def remove_language(self, language: Language):
         # remove a language from the benchmark suite
         pass
 
-    def remove_languages(self, languages: List[str]):
+    def remove_languages(self, languages: List[Language]):
         # remove multiple languages from the benchmark suite
         pass
 
---
+++ src/language.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class Language(Enum):
+    DE = 1
+    EN = 2
