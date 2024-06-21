d56c20559c6c14bd4f5427341189946bafd14e87
Carolin <carolin.ganahl@tum.de>
2024-06-21 11:23:43 +0200

    alexsis added

---
+++ src/utils/alexsis_data_provider.py
@@ -0,0 +1,33 @@
+# Preprocess the Alexsis dataset into a numpy array
+import numpy as np
+import pandas as pd
+import numpy as np
+from collections import Counter
+from collections import defaultdict
+
+class AlexsisDataProvider():
+
+    def provide_data_as_numpy_array(self):
+        data = np.genfromtxt('data/alexsis/ALEXSIS.tsv', delimiter='\t', dtype=None, encoding=None)
+        substitutes = data[:, -25:]
+        synonyms_as_list = [row.tolist() for row in substitutes]
+        
+        sorted_lists = []
+        for synonyms_list in synonyms_as_list:
+            word_counts = Counter(synonyms_list)
+            freq_dict = defaultdict(list)
+            for word, count in word_counts.items():
+                freq_dict[count].append(word)
+            sorted_freq_dict = dict(sorted(freq_dict.items(), reverse=True))
+            sorted_lists.append(sorted_freq_dict)
+
+        processed_data = []
+        for index, data_values in enumerate(data):
+            processed_line = [str(data_values[0]), str(data_values[1]), None,sorted_lists[index]]
+            processed_data.append(processed_line)
+
+        return np.array(processed_data, dtype=object)
+
+data = AlexsisDataProvider().provide_data_as_numpy_array()
+print(data[0])
+
