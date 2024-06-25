e429391bb77b229ed015b28e315d3b4e5da8dbab
Carolin <carolin.ganahl@tum.de>
2024-06-25 15:04:22 +0200

    added gptwrapper

---
+++ gpt_wrapper.py
@@ -0,0 +1,23 @@
+import openai # Import the OpenAI library
+import src.utils.nnseval_data_provider as nnseval_data_provider
+from src.config import OPENAI_API_KEY
+
+openai.api_key = OPENAI_API_KEY
+content = None
+
+original_data = nnseval_data_provider.NNSevalDataProvider().provide_data_as_numpy_array()
+
+completion = openai.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "Translator for a Lexical simplification dataset"},
+        {"role": "user", "content": "Translate the following dataset from English to German, keep the same format: " + str(original_data)}
+    ]
+)
+response = str(completion.choices[0].message.content)
+
+# Save response in a text file
+with open('response.txt', 'w') as file:
+        file.write(response)
+
+
